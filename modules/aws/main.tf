6672e9ad8587ba05a96d4bfd7a9a588306784d58
Mahmoud Ismail <mahmoud@logicalclocks.com>
2021-06-16 16:21:13 +0200

    public_access_block should depend on the bucket

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -59,7 +59,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block_bucket" {
-  bucket                  = local.bucket_name
+  bucket                  = aws_s3_bucket.bucket.id
   block_public_acls       = true
   block_public_policy     = true
   restrict_public_buckets = true
